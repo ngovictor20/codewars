63e937fc44cd199c05718134b5d7e1ae6c7207b7
Victor Ngo <ngo.victor20@gmail.com>
2022-01-01 11:41:04 -0500

    Another commit

--- sudokuValidator.js
+++ sudokuValidator.js
@@ -1,22 +1,14 @@
 function validSolution(board) {
     const transposedBoard = board[0].map((_, colIndex) => board.map(row => row[colIndex]));
     const sudokuPass = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    // console.log(transposedBoard)
-    board.every((row) => {
-        console.log(row);
-        if (row.includes(0)) return false;
-        if (!sudokuPass.every((num) => row.includes(num))) {
-            return false;
+    const boardPass = board.every((row) => !row.includes(0) && sudokuPass.every((num) => row.includes(num)));
+    const transposedPass = transposedBoard.every((row) => sudokuPass.every((num) => row.includes(num)));
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+
         }
-    })
-    console.log("passes this");
-    transposedBoard.forEach((row) => {
-        console.log(row, sudokuPass.every((num) => row.includes(num)));
-        if (!sudokuPass.every((num) => row.includes(num))) {
-            return false;
-        }
-    })
-    return true;
+    }
+    return boardPass && transposedPass;
 }
 
 module.exports = validSolution;
